65fa3fff8d8c0bef60d1307161b8b3156efa864c
phuongnam <namtp@production.com.vn>
2021-10-22 18:42:46 +0700

    -BaiTapZalo

--- src/index.js
+++ src/index.js
@@ -6,43 +6,71 @@ import {
   ScrollView,
   Image,
   TouchableOpacity,
+  FlatList,
 } from 'react-native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import Entypo from 'react-native-vector-icons/Entypo';
 
 const users = [
-  'captain',
-  'ironman',
-  'blackwindow',
-  'spiderman',
-  'yasuo',
-  'wanda',
-  'wonderwoman',
-  'suppergirl',
-  'strange',
-  'flash',
+  {
+    name: 'captain',
+    mutual_friend_count: 24,
+    image: require('./images/captain.jpg'),
+  },
+  {
+    name: 'ironman',
+    mutual_friend_count: 24,
+    image: require('./images/ironman.jpeg'),
+  },
+  {
+    name: 'blackwindow.jpeg',
+    mutual_friend_count: 24,
+    image: require('./images/blackwidow.jpg'),
+  },
+  {
+    name: 'spiderman',
+    mutual_friend_count: 24,
+    image: require('./images/spiderman.jpeg'),
+  },
+  {
+    name: 'yasuo',
+    mutual_friend_count: 24,
+    image: require('./images/yasuo.jpg'),
+  },
+  {
+    name: 'wanda',
+    mutual_friend_count: 24,
+    image: require('./images/wanda.jpg'),
+  },
+  {
+    name: 'strange',
+    mutual_friend_count: 24,
+    image: require('./images/strange.jpg'),
+  },
+  {
+    name: 'wonderwoman',
+    mutual_friend_count: 24,
+    image: require('./images/wonderwoman.jpg'),
+  },
+  {
+    name: 'suppergirl',
+    mutual_friend_count: 24,
+    image: require('./images/spiderman.jpeg'),
+  },
+  {
+    name: 'flash',
+    mutual_friend_count: 24,
+    image: require('./images/spiderman.jpeg'),
+  },
 ];
 
 export default class Home extends Component {
   userSearchList = [...users]
     .sort(() => Math.random() - Math.random())
     .slice(0, 8);
-  //   userSearchList = [
-  //     'captain',
-  //     'ironman',
-  //     'blackwindow',
-  //     'spiderman',
-  //     'yasuo',
-  //     'wanda',
-  //     'wonderwoman',
-  //     'suppergirl',
-  //     'strange',
-  //     'flash',
-  //   ];
-  imagePath = imageName => {
-    return `./images/ironman.jpg`;
-  };
+
   render() {
+    console.log(this.userSearchList);
     return (
       <View>
         <View style={{marginBottom: 20}}>
@@ -51,24 +79,13 @@ export default class Home extends Component {
             <Text style={styles.title}>Danh sách tìm kiếm gần đây</Text>
           </View>
           <View>
-            <ScrollView
-              horizontal={true}
-              style={{marginVertical: 20}}
-              contentContainerStyle={{
-                justifyContent: 'center',
-                flexDirection: 'row',
-                paddingHorizontal: 20,
-              }}>
-              {this.userSearchList.map((user, index) => (
-                <View key={index}>
-                  <Image
-                    style={styles.userImage}
-                    source={require('./images/ironman.jpeg')}
-                  />
-                  <Text>{user}</Text>
-                </View>
-              ))}
-            </ScrollView>
+            <FlatList
+              data={this.userSearchList}
+              horizontal
+              style={{flexGrow: 0}}
+              renderItem={({user}) => <UserSearch user={user} />}
+              keyExtractor={(user, index) => `${user.name}_${index}`}
+            />
           </View>
         </View>
         <View>
@@ -77,6 +94,32 @@ export default class Home extends Component {
             <Text style={styles.title}>Gợi ý kết bạn</Text>
           </View>
         </View>
+      </View>
+    );
+  }
+}
+
+class UserSearch extends Component {
+  render() {
+    const {user} = this.props;
+    console.log(this.props);
+    return (
+      <View>
+        {/* <Image
+          style={styles.userImage}
+          source={user.image}
+        /> */}
+        <Text>{user}</Text>
+      </View>
+    );
+  }
+}
+
+class UserSuggest extends Component {
+  render() {
+    return (
+      <View>
+        <Image />
       </View>
     );
   }
